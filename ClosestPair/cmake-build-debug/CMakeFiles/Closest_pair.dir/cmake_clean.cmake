9c2dce334fddc33a909bcebfd20279b201864237
anthonycortes <Anthony.Cortes94@gmail.com>
2019-02-21 18:16:20 -0600

    move files around

---
+++ ClosestPair/cmake-build-debug/CMakeFiles/Closest_pair.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Closest_pair.dir/main.cpp.o"
+  "Closest_pair.pdb"
+  "Closest_pair"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Closest_pair.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
