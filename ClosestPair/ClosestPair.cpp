f8fe44d3baaefa02565ff1a83d2af88ed4c66728
anthonycortes <Anthony.Cortes94@gmail.com>
2019-02-23 10:43:52 -0600

    fix closest split pair bug when size=4

--- ClosestPair/ClosestPair.cpp
+++ ClosestPair/ClosestPair.cpp
@@ -1,5 +1,6 @@
 // New comment
 #include <utility>
+#include <algorithm>
 #include <float.h>
 #include <random>
 #include <ctime>
@@ -59,15 +60,19 @@ class Solution {
     // get the minimum distance from the two closest points
     auto lDistance = distance(closeL.first, closeL.second);
     auto rDistance = distance(closeR.first, closeR.second);
+
+    // smallest distance, left pair or right pair
     double delta = std::min(lDistance, rDistance);
 
     // find the closest split pair
     std::pair<Point, Point> closeSp = closest_split_pair(Px, Py, delta);
     auto spDistance = distance(closeSp.first, closeSp.second);
 
-    if (spDistance < delta) {
+    if (spDistance < delta){
+      // split distance is better than either half
       return closeSp;
     } else {
+      // determine which half has the closer pair
       return lDistance < rDistance ? closeL : closeR;
     }
 
@@ -77,11 +82,11 @@ class Solution {
     
     // get largest x coord in left half
     auto pxSize = Px.size();
-    auto xBar = Px[(Px.size() - 1) / 2];
+    auto xBar = Px[(Px.size() - 1) / 2].x;
 
     // determine min and max values
-    auto min = xBar.x - delta;
-    auto max = xBar.x + delta;
+    auto min = xBar - delta;
+    auto max = xBar + delta;
 
     // get point whos x-coordinate is xBar +- delta
     std::vector<Point> Sy;
@@ -97,7 +102,7 @@ class Solution {
 
     int l = Sy.size();
     for (int i = 0; i < (l - 1); ++i) {
-      for (int j = i + 1; j < std::min(7, l-1); ++j) {
+      for (int j = i + 1; j < std::min(7, l); ++j) {
         auto currentDist = distance(Sy[i], Sy[j]);
         if (currentDist < best) {
           best = currentDist;
