15f3b024750b00b39213fa03fe652d228c9915a6
anthonycortes <Anthony.Cortes94@gmail.com>
2019-02-27 13:25:16 -0600

    add files

---
+++ RecursiveBST/cmake-build-debug/CMakeFiles/RecursiveBST.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RecursiveBST.dir/main.cpp.o"
+  "RecursiveBST.pdb"
+  "RecursiveBST"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RecursiveBST.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
