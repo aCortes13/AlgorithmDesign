021d1699d9dc02520304d8a16791af57b2a99fe0
anthonycortes <Anthony.Cortes94@gmail.com>
2019-02-26 18:26:28 -0600

    file initialization

---
+++ MatchingParenthesis/cmake-build-debug/CMakeFiles/MatchingParenthesis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatchingParenthesis.dir/main.cpp.o"
+  "MatchingParenthesis.pdb"
+  "MatchingParenthesis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatchingParenthesis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
