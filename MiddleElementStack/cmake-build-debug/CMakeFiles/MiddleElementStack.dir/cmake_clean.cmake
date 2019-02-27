c38d26614a1f5062fe2b475fcbe245ef6210a7fb
anthonycortes <Anthony.Cortes94@gmail.com>
2019-02-26 20:20:33 -0600

    init files

---
+++ MiddleElementStack/cmake-build-debug/CMakeFiles/MiddleElementStack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiddleElementStack.dir/main.cpp.o"
+  "MiddleElementStack.pdb"
+  "MiddleElementStack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiddleElementStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ MiddleElementStack/deleteMid.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Greg LaKomski on 2/21/19.
+//
+
+
+#include<iostream>
+#include<cmath>
+#include<cstdio>
+#include<stack>
+
+class DeleteMid{
+
+ public:
+
+  void deleteMid(std::stack<int> &mystack, int n, int curr=0){
+
+                  // If stack is empty or all items
+                  // are traversed
+   
+   //...........code
+
+                  // Remove current item
+    
+   // .......... code
+
+                  // Remove other items
+                  
+   // .......... code
+
+                  // Put all items back except middle
+   // .......... code
+
+
+  }
+};
