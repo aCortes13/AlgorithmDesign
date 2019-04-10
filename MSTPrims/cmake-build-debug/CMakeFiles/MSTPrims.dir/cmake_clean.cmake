755257ab9ebf017c3e91c39aa25d5cbeec851c35
anthonycortes <Anthony.Cortes94@gmail.com>
2019-04-09 21:20:46 -0500

    adding prims

---
+++ MSTPrims/cmake-build-debug/CMakeFiles/MSTPrims.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MSTPrims.dir/main.cpp.o"
+  "MSTPrims.pdb"
+  "MSTPrims"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MSTPrims.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
