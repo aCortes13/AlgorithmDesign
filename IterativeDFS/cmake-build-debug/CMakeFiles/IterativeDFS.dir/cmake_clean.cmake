186185d846f0fa89d347cd358f0105fa3a5eaea6
anthonycortes <Anthony.Cortes94@gmail.com>
2019-04-09 22:42:03 -0500

    adding DFS

---
+++ IterativeDFS/cmake-build-debug/CMakeFiles/IterativeDFS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IterativeDFS.dir/main.cpp.o"
+  "IterativeDFS.pdb"
+  "IterativeDFS"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IterativeDFS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
