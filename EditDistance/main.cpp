5631b8c5292f956a8ec5e05df412139ccb7a964b
anthonycortes <Anthony.Cortes94@gmail.com>
2019-05-06 10:42:36 -0500

    updaing

--- EditDistance/main.cpp
+++ EditDistance/main.cpp
@@ -1,6 +1,17 @@
+// operations to convert str1 to str2
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include "EditDistanceDynamic.h"
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+// Driver program
+int main(){
+  // your code goes here
+  std::string str1 = "sunday";
+  std::string str2 = "saturday";
+
+  std::cout << editDist( str1 , str2);
+
   return 0;
 }
